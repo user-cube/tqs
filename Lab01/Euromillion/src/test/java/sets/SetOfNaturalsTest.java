9f78cea6d762df174cc3acb87433ce7a8dbef7a6
Rui Coelho <ruicoelho@ua.pt>
2020-02-14 22:41:32 +0000

    code style

--- Lab01/Euromillion/src/test/java/sets/SetOfNaturalsTest.java
+++ Lab01/Euromillion/src/test/java/sets/SetOfNaturalsTest.java
@@ -9,7 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
 
 /**
  * @author ico0
