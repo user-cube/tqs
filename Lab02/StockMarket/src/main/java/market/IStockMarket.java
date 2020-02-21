f33fce8a5a57b55ebdf0ae6a7d5788b51412cb6c
Rui Coelho <ruicoelho@ua.pt>
2020-02-21 12:43:48 +0000

    Implementation of StockMarket

---
+++ Lab02/StockMarket/src/main/java/market/IStockMarket.java
@@ -0,0 +1,5 @@
+package market;
+
+public interface IStockMarket {
+    double getPrice(String price);
+}
---
+++ Lab02/StockMarket/src/main/java/market/Stock.java
@@ -0,0 +1,28 @@
+package market;
+
+public class Stock {
+
+    public String name;
+    public int quantity;
+
+    public Stock(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
