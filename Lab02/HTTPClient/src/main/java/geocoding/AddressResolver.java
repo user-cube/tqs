4b29cbc2f57518e8fb22cd82f711d8d107cd7d57
Rui Coelho <ruicoelho@ua.pt>
2020-02-23 00:57:05 +0000

    Fix project; Tests

--- Lab02/HTTPClient/src/main/java/geocoding/AddressResolver.java
+++ Lab02/HTTPClient/src/main/java/geocoding/AddressResolver.java
@@ -1,12 +1,11 @@
 package geocoding;
-import connection.TqsHttpClient;
 
+import connection.TqsHttpClient;
 import org.apache.http.client.utils.URIBuilder;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Formatter;
 import java.util.Locale;
@@ -18,7 +17,8 @@ public class AddressResolver {
         this.httpClient = httpClient;
     }
 
-    public Address findAddressForLocation(double latitude, double longitude) throws URISyntaxException, IOException, org.json.simple.parser.ParseException {
+    public Address findAddressForLocation(double latitude, double longitude) throws URISyntaxException, org.json.simple.parser.ParseException {
+
         URIBuilder uriBuilder = new URIBuilder(
                 "http://open.mapquestapi.com/geocoding/v1/reverse?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ");
         uriBuilder.addParameter("location",
@@ -37,10 +37,10 @@ public class AddressResolver {
         obj = (JSONObject) ((JSONArray) obj.get("results")).get(0);
         JSONObject address = (JSONObject) ((JSONArray) obj.get("locations")).get(0);
 
-        String road  = (String) address.get("street");
-        String city  = (String) address.get("adminArea5");
+        String road = (String) address.get("street");
+        String city = (String) address.get("adminArea5");
         String state = (String) address.get("adminArea3");
-        String zip   = (String) address.get("postalCode");
+        String zip = (String) address.get("postalCode");
         return new Address(null, road, city, state, zip);
     }
 }
---
+++ Lab02/HTTPClient/src/test/java/geocoding/AddressResolverTest.java
@@ -0,0 +1,49 @@
+package geocoding;
+
+import connection.TqsHttpClient;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+
+@ExtendWith(MockitoExtension.class)
+public class AddressResolverTest {
+    @Mock
+    private TqsHttpClient tqsHttpClient;
+
+    @Test
+    public void findAddressForLocation() throws URISyntaxException, ParseException {
+        String simpleURL = "http://open.mapquestapi.com/geocoding/v1/reverse?key=uXSAVwYWbf9tJmsjEGHKKAo0gOjZfBLQ&location=40.633331%2C-8.659713&includeRoadMetadata=true";
+        JSONObject jsonObject = null;
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("src/test/java/geocoding/simpleJSON.json"));
+            jsonObject = (JSONObject) obj;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Mockito.when(tqsHttpClient.get(simpleURL)).thenReturn(jsonObject.toString());
+        AddressResolver resolver = new AddressResolver(tqsHttpClient);
+        Address result = resolver.findAddressForLocation(40.6333308, -8.659713);
+
+        Assert.assertEquals(result,
+                new Address(null,
+                        "DETI - Departamento de Electrónica, Telecomunicações e Informática",
+                        "Glória e Vera Cruz",
+                        "Centro",
+                        "3810-193")
+        );
+    }
+}
