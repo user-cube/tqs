d1773186b15c84bf634a599fe7a98dff643856f5
Rui Coelho <ruicoelho@ua.pt>
2020-02-28 12:53:36 +0000

    remove imports

--- Lab03/SeleniumIDE/Ex1Test.java
+++ Lab03/SeleniumIDE/Ex1Test.java
@@ -2,25 +2,13 @@
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.Keys;
 import java.util.List;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class Ex1Test {
   private WebDriver driver;
